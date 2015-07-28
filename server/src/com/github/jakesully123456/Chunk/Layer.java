43f97f3294afaff6e000c45c1967013d69d3356a
Sam Sully <samuel.sully@live.com>
2015-07-28 08:32:59 +0100

    Added Borough Generator :+1:
    The first build of the borough generator works. Additionally, I have
    setup the image for the borough generator and the JSON file with all the
    borough numbers and corresponding names.

---
+++ server/src/com/github/jakesully123456/Chunk/Layer.java
@@ -0,0 +1,9 @@
+package com.github.jakesully123456.Chunk;
+
+public class Layer {
+
+	public Layer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ server/src/com/github/jakesully123456/Main/CityVisServer.java
@@ -0,0 +1,20 @@
+package com.github.jakesully123456.Main;
+
+import com.github.jakesully123456.Generation.BoroughGen;
+
+public class CityVisServer {
+
+	public static void main(String[] args) {
+		BoroughGen gen = new BoroughGen();
+		/*for (int x = 0; x < gen.map.length; x++) {
+			int count = 0;
+			for (int z = 0; z < gen.map[x].length; z++) {
+				System.out.print(gen.map[x][z]);
+				count++;
+				if (count == 500) {
+					System.out.print("\n");
+				}
+			}
+		}*/
+	}
+}

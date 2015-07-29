313b94257506aa22d01149d08262ab636a806920
JCTyblaidd <james@tyblaidd.co.uk>
2015-07-29 22:21:22 +0100

    MOAR stuff - client side of communication
    Prepare for the monolith of json files that is sams server

--- client/new/php_server.bat
+++ client/new/php_server.bat
@@ -1,3 +1,6 @@
 start "" http://127.0.0.1:6666
+start cmd /c "ping 127.0.0.1:6666 && exit"
+start "" cmd /c "echo PHP-Server-Boot-INIT&echo(&pause"
+start "" cmd /c "echo Loaded - index should be loaded&echo(&pause"
 php -S 127.0.0.1:6666
 pause

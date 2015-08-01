8c5a0affcd8d505c3a5f44fc168ba2543e95f576
Sam Sully <samuel.sully@live.com>
2015-08-01 09:06:57 +0100

    Minor change.

--- webserver/index.php
+++ webserver/index.php
@@ -1,36 +1,36 @@
 		<?php
 			if ($_SERVER['QUERY_STRING'] == "crimedata") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ crimedata 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ crimedata 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "wardlocs") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ wardlocs 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ wardlocs 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "wardlist") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ wardlist 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ wardlist 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "firedata") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ firedata 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ firedata 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "pricedata") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ pricedata 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ pricedata 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "boroughmap") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ boroughmap 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ boroughmap 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "updatecrimedata") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ updatecrimedata 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ updatecrimedata 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "debug") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ debug 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ debug 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "fireimg") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ fireimg 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ fireimg 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "priceimg") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ priceimg 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ priceimg 2>&1`;
 				echo $output;
 			} else if ($_SERVER['QUERY_STRING'] == "mapimg") {
-				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Desktop/YRS/YRSCityVis/server/ mapimg 2>&1`;
+				$output = `java -jar C:/Users/Samuel/Code/YRS/YRSCityVis/CityVisServer.jar C:/Users/Samuel/Code/YRS/YRSCityVis/server/ mapimg 2>&1`;
 				echo $output;
 			}
 		?>
